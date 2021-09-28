bedcfe6bc9c7c48f3b130da1af6743f4920b3aee
paquisha <paquishaxd@gmail.com>
2021-09-27 21:41:56 -0500

    se crea estructura y primera clase de laboratorio final

---
+++ mundoPc/src/ec/aekmot/mundopc/Monitor.java
@@ -0,0 +1,57 @@
+package ec.aekmot.mundopc;
+
+public class Monitor {
+	private int idMonitor;
+	private String marca;
+	private double tamanio;
+	private int contadorMonitores;
+	
+	private Monitor() {
+		
+	}
+	
+	private Monitor(String marca, double tamanio) {
+		this.marca = marca;
+		this.tamanio = tamanio;
+	}
+
+	public int getIdMonitor() {
+		return idMonitor;
+	}
+
+	public void setIdMonitor(int idMonitor) {
+		this.idMonitor = idMonitor;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getTamanio() {
+		return tamanio;
+	}
+
+	public void setTamanio(double tamanio) {
+		this.tamanio = tamanio;
+	}
+
+	public int getContadorMonitores() {
+		return contadorMonitores;
+	}
+
+	public void setContadorMonitores(int contadorMonitores) {
+		this.contadorMonitores = contadorMonitores;
+	}
+
+	@Override
+	public String toString() {
+		return "Monitor [idMonitor=" + idMonitor + ", marca=" + marca + ", tamanio=" + tamanio + ", contadorMonitores="
+				+ contadorMonitores + "]";
+	}
+	
+	
+}
