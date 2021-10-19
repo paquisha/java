568ecd017d1d5a57f38f41a808eacc3f3645ffec
paquisha <paquishaxd@gmail.com>
2021-10-19 07:17:42 -0500

    se crea laboratorio final de catalogo de peliculas

---
+++ CatalogoPeliculas/src/ec/com/aekmot/peliculas/excepciones/AccesoDatosEx.java
@@ -0,0 +1,7 @@
+package ec.com.aekmot.peliculas.excepciones;
+
+public class AccesoDatosEx extends Exception{
+	public AccesoDatosEx(String mensaje) {
+		super (mensaje);
+	}
+}
---
+++ CatalogoPeliculas/src/ec/com/aekmot/peliculas/excepciones/LecturaDatosEx.java
@@ -0,0 +1,10 @@
+package ec.com.aekmot.peliculas.excepciones;
+
+public class LecturaDatosEx extends AccesoDatosEx{
+
+	public LecturaDatosEx(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
