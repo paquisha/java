53e398ff34f40c433b1c02f0f1d85dead86bcfdf
paquisha <paquishaxd@gmail.com>
2021-12-06 17:24:27 -0500

    se agrega primer metodo y pruba para el ide

--- FactorialNumero/src/FactorialNumero.java
+++ FactorialNumero/src/FactorialNumero.java
@@ -1,5 +1,21 @@
 public class FactorialNumero {
     public static void main(String[] args) {
-        
+
+    }
+
+    //metodo recursivo
+    public int factorialRecursivo(int n){
+        //validamos
+        if(n < 0){
+            return 0;
+        }else{
+            //caso base
+            if(n == 0){
+                return 1;
+            }else{
+                //problema-1(dominio)
+                return n * factorialRecursivo(n - 1);
+            }
+        }
     }
 }
