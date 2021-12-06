9e9d1d8db4ecdd7ab05ece88d82abe7a039517dd
paquisha <paquishaxd@gmail.com>
2021-12-06 17:43:15 -0500

    se finaliza ejercicio de factorial mediante recursividad

--- FactorialNumero/src/FactorialNumero.java
+++ FactorialNumero/src/FactorialNumero.java
@@ -1,6 +1,11 @@
 public class FactorialNumero {
     public static void main(String[] args) {
-
+        FactorialNumero prueba = new FactorialNumero();
+        System.out.println("El factorial de 4 mediante metodo recursivo es: " +
+                prueba.factorialRecursivo(4));
+        //impresion por ciclo
+        System.out.println("El factorial de 4 mediante metodo de ciclo es: " +
+                prueba.factorialCiclo(4));
     }
 
     //metodo recursivo
@@ -16,6 +21,20 @@ public class FactorialNumero {
                 //problema-1(dominio)
                 return n * factorialRecursivo(n - 1);
             }
+        }
+    }
+
+    //metodo factorial mediante un ciclo
+    public int factorialCiclo(int n){
+        int factor = 1;
+        if(n < 0){
+            return 0;
+        }else{
+            while(n != 0){
+                factor = n * factor;
+                n--;
+            }
+            return factor;
         }
     }
 }
