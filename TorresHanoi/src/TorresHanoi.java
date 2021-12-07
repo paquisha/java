969a1ddd25c59f5c573d9340fd4f07812188200e
paquisha <paquishaxd@gmail.com>
2021-12-06 23:21:31 -0500

    fin de ejercicio torres de hannoi, que bacna que es esto

--- TorresHanoi/src/TorresHanoi.java
+++ TorresHanoi/src/TorresHanoi.java
@@ -1,2 +1,19 @@
 public class TorresHanoi {
+    public static void main(String[] args) {
+        TorresHanoi prueba = new TorresHanoi();
+        prueba.torresHanoi(3,1,2,3);
+    }
+
+    //metodo recursivo
+    public void torresHanoi(int discos, int torre1, int torre2, int torre3){
+        //caso base
+        if(discos == 1){
+            System.out.println("Mover discos de torre " + torre1 + " a torre " + torre3);
+        }else{
+            //dominio
+            torresHanoi(discos-1, torre1, torre3, torre2);
+            System.out.println("Mover discos de torre " + torre1 + " a torre " + torre3);
+            torresHanoi(discos-1, torre2, torre1, torre3);
+        }
+    }
 }
