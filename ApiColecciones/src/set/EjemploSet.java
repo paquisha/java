46a317a5664ca2a70c0e4cadb6e354add425d132
aekmot <paquishaxd@gmail.com>
2022-01-07 16:16:47 -0500

    Ejemplo con linkedSet

--- ApiColecciones/src/set/EjemploSet.java
+++ ApiColecciones/src/set/EjemploSet.java
@@ -1,4 +1,43 @@
 package set;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class EjemploSet {
+    public static void main(String[] args) {
+        //creamos variable de tipo Set, utilizando la clase mas comun que es hashSet
+        Set miSet = new HashSet();
+        //utilizamos metodo add del api de collections
+        miSet.add(miSet);
+        //agregamos los dias de la semana, pero puede recibir
+        //cualquier tipo object
+        miSet.add("Lunes");
+        miSet.add("Martes");
+        miSet.add("Miercoles");
+        miSet.add("jueves");
+        miSet.add("Viernes");
+        miSet.add("Sabado");
+        miSet.add("Domingo");
+        //imprimir(miSet);
+
+        //creamos una variable para linkedset
+        LinkedHashSet linkedSet = new LinkedHashSet();
+        linkedSet.add("Lunes");
+        linkedSet.add("Martes");
+        linkedSet.add("Miercoles");
+        linkedSet.add("Jueves");
+        linkedSet.add("Viernes");
+        linkedSet.add("Sabado");
+        linkedSet.add("Domingo");
+        imprimir(linkedSet);
+    }
+    //creamos metodo para recibir la lista
+    public static void  imprimir(Collection coleccion){
+        //vamos usar una expresion lambda, para iterar los elementos
+        coleccion.forEach(elemento ->{
+            System.out.println("elemento: " + elemento);
+        });
+    }
 }
