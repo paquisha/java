9bad91516cdc5821883e846f4b98d2f1bb5a2f41
aekmot <paquishaxd@gmail.com>
2022-01-27 12:13:19 -0500

    Se agrega ejercicio de punto de ventas

---
+++ ProyectoVentas/src/proyectoventas/ProyectoVentas.java
@@ -0,0 +1,13 @@
+package proyectoventas;
+
+/**
+ *
+ * @author aekmot
+ */
+public class ProyectoVentas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
